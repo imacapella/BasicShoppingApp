de82f6326de9bc1a9430c71cd1ea5b45a84b0439
imacapella <gurkankaradas42@gmail.com>
2024-09-28 17:23:14 +0300

    Initial Commit

---
+++ BasicShoppingApp/BasicShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicShoppingAppApp.swift
+//  BasicShoppingApp
+//
+//  Created by Gürkan Karadaş on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BasicShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
